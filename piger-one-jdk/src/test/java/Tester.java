299f1c7ada68a89d9446a26264005a4d9804d976
pigercc.liang <pigercc@gmail.com>
2017-05-10 12:22:34 +0800

    init

---
+++ piger-one-jdk/src/test/java/Tester.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Created by pigercc.liang on 2017/5/6.
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+        C1 c1=new C1();
+        System.out.println(c1.getBigDecimal());
+    }
+}
+
+
+class  C1 implements Serializable
+{
+    private BigDecimal bigDecimal;
+
+    public BigDecimal getBigDecimal() {
+        return bigDecimal;
+    }
+
+    public void setBigDecimal(BigDecimal bigDecimal) {
+        this.bigDecimal = bigDecimal;
+    }
+}
